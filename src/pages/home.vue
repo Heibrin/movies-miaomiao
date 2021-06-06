13d5675df64effa1a133f3c65e284f7bad9038ae
Heibrin <liqing0715@163.com>
2021-06-06 15:30:50 +0800

    init

---
+++ src/pages/home.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="home">
+        <!-- 头部 -->
+        <div class="header">
+            <div class="header-inner">
+                <!-- logo -->
+                <h1>
+                    <a href="javascript(voiod:0)" class="logo">喵喵电影</a>
+                </h1>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'home',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+.logo{
+    float: left;
+    width: 133px;
+    height: 80px;
+    line-height: 100em;
+    text-indent: -10000px;
+    overflow: hidden;
+    background-image: url(../assets/logo-movies.png);
+    background-repeat: no-repeat;
+}
+</style>
